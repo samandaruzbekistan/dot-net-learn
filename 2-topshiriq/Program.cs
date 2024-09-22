1fdfe7f350cb6630e23ee7ad6dc4e62089c9defa
Samandar Sariboyev <samandarsariboyev69@gmail.com>
2024-09-22 19:09:05 +0500

    2 - topshiriq

---
+++ 2-topshiriq/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+Console.Write("Birinchi raqamni kiriting: ");
+double raqam1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Amalni kiriting (+, -, *, /): ");
+char amal = Convert.ToChar(Console.ReadLine());
+
+Console.Write("Ikkinchi raqamni kiriting: ");
+double raqam2 = Convert.ToDouble(Console.ReadLine());
+
+
+double natija = 0;
+switch (amal)
+{
+    case '+':
+        natija = raqam1 + raqam2;
+        break;
+    case '-':
+        natija = raqam1 - raqam2;
+        break;
+    case '*':
+        natija = raqam1 * raqam2;
+        break;
+    case '/':
+        if (raqam2 != 0)
+        {
+            natija = raqam1 / raqam2;
+        }
+        else
+        {
+            Console.WriteLine("Nolga bo'lib bo'lmaydi!");
+            return;
+        }
+        break;
+    default:
+        Console.WriteLine("Noto'g'ri amal kiritildi.");
+        return;
+}
+
+
+Console.WriteLine($"Natija: {natija}");
+
+
+Console.Write("Musbat butun sonni kiriting (N): ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+if (N <= 0)
+{
+    Console.WriteLine("Iltimos, musbat butun son kiriting.");
+    return;
+}
+
+int yigindi = 0;
+for (int i = 1; i <= N; i++)
+{
+    yigindi += i;
+}
+
+Console.WriteLine($"1 dan {N} gacha bo'lgan sonlar yig'indisi: {yigindi}");
+
+
+
+
+
+Console.Write("Raqam kiriting: ");
+int raqam = Convert.ToInt32(Console.ReadLine());
+
+if (raqam % 2 == 0)
+{
+    Console.WriteLine("Juft");
+}
+else
+{
+    Console.WriteLine("Toq");
+}
