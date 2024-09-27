afc84ebbb74358b0584cf22cfb92ee2e3592d5a8
Samandar Sariboyev <79693410+samandaruzbekistan@users.noreply.github.com>
2024-09-27 09:45:34 +0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,15 +1,13 @@
 ﻿using System;
 
-string inputRadius = Console.ReadLine();
-decimal radius = Convert.ToDecimal(inputRadius);
-decimal s = 3.14m*radius*radius; 
-decimal l = 2*3.14m*radius; 
-Console.WriteLine($"S={s}, L={l}");
+string inputNumber = Console.ReadLine();
+int number = Convert.ToInt32(inputNumber);
 
-string inputValue = Console.ReadLine();
-double sum = Convert.ToInt32(inputValue) * 12400;
-Console.WriteLine($"{sum} so'm");
-
-int age = Convert.ToInt32(Console.ReadLine());
-int day = 365*age;
-Console.WriteLine(day);
+string str = "teLefOn";
+int length = str.Length;
+if(length < number){
+    Console.WriteLine(str.ToUpper());
+}
+else{
+    Console.WriteLine(str.ToLower());
+}
