dc3865ba7a0d83998015be0dbced4d505c5a46cc
Samandar Sariboyev <79693410+samandaruzbekistan@users.noreply.github.com>
2024-09-27 10:05:03 +0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -11,3 +11,48 @@ if(length < number){
 else{
     Console.WriteLine(str.ToLower());
 }
+
+
+// 2 masala
+
+int x = 15;
+int y = 10;
+
+string result;
+if (x > y)
+{
+    result = "x is greater than y";
+}
+else if (x < y)
+{
+    result = "x is less than y";
+}
+else if (x == y)
+{
+    result = "x is equal to y";
+}
+else
+{
+    result = "x and y are not comparable";
+}
+
+Console.WriteLine(result);
+
+
+
+// 3 masala
+
+string uzbekDayName = Console.ReadLine();
+string englishDayName = uzbekDayName switch
+{
+    "Dushanba" => "Monday",
+    "Seshanba" => "Tuesday",
+    "Chorshanba" => "Wednesday",
+    "Payshanba" => "Thursday",
+    "Juma" => "Friday",
+    "Shanba" => "Saturday",
+    "Yakshanba" => "Sunday",
+    _ => "Hafta kuni aniqlanmadi"
+};
+Console.WriteLine(englishDayName);
+
